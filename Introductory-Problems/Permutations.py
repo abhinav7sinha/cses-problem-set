d44162ba358ce34da1717a11843eb679d5e5cd6d
Sinha, Abhinav <sinhabhi@iu.edu>
2023-04-19 07:34:17 -0400

    4/19

--- Introductory-Problems/Permutations.py
+++ Introductory-Problems/Permutations.py
@@ -1,36 +1,13 @@
 n = int(input())
 
-ans = None
-'''
-recursive method to generate all permutations
-when a permutation is generated -> check if it is beautiful
-if yes -> store it in ans and return from the function
-'''
-nums = [num for num in range(1,n+1)]
-ans = None
-numSet = set(nums)
-
-def gen(ans_r, numSet_r):
-    global ans
-    if len(numSet_r)==0:
-        ans = ' '.join(str(i) for i in ans_r)
-        return True
-    t = numSet_r.copy()
-    for num in t:
-        if len(ans_r)>0:
-            if abs(ans_r[-1]-num)==1:
-                continue
-        # DO
-        numSet_r.remove(num)
-        ans_r.append(num)
-        # OP
-        if gen(ans_r, numSet_r):
-            return True
-        # UNDO
-        numSet_r.add(num)
-        ans_r.pop()
-
-if gen([], numSet):
-    print(ans)
+if n==1:
+    print(n)
+elif n>3:
+    numli = []
+    for i in range(2, n+1, 2):
+        numli.append(i)
+    for i in range(1, n+1, 2):
+        numli.append(i)
+    print(' '.join(str(i) for i in numli))
 else:
     print("NO SOLUTION")
