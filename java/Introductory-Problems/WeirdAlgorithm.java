f0dedfd4750dc5d5db47e3ed0c1402329fa3cd9c
Sinha, Abhinav <sinhabhi@iu.edu>
2023-05-06 17:25:44 -0400

    java 5/6

---
+++ java/Introductory-Problems/WeirdAlgorithm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long n = s.nextLong();
+		System.out.print(n+" ");
+		while (n!=1) {
+			if (n%2==0) {
+				n/=2;
+			} else {
+				n*=3;
+				n+=1;
+			}
+			System.out.print(n+" ");
+		}
+	}
+}
