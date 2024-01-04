855bf7322aefcdcfa352b01b3201731e8f474be6
Sinha, Abhinav <sinhabhi@iu.edu>
2024-01-04 09:13:22 -0500

    fix resource leak

--- java/Introductory-Problems/WeirdAlgorithm.java
+++ java/Introductory-Problems/WeirdAlgorithm.java
@@ -14,5 +14,6 @@ public class WeirdAlgorithm {
 			}
 			System.out.print(n+" ");
 		}
+		s.close();
 	}
 }
--- java/Sorting-and-Searching/MovieFestival.java
+++ java/Sorting-and-Searching/MovieFestival.java
@@ -1,7 +1,6 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
